4674229894be3d7d70a71437fdad94be83ff0303
MartinsMessias <messiasads2017@gmail.com>
2021-04-04 20:52:33 -0500

    Added api to call newsletters

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-import redis
+#import redis
 from flask import Flask
+from flask.json import jsonify
 from src.newsletter.news_letter import newsletter
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 
-cache = redis.from_url(os.environ.get("REDISTOGO_URL"))
+#cache = redis.from_url(os.environ.get("REDISTOGO_URL"))
 
 @app.route('/', methods=['GET'])
 def index():
-    newsletters = newsletter()
-
-    return jsonify({'newsletter':newsletter})
+    #newsletters = newsletter()
+    return jsonify({'newsletter':'newsletter'})
 
 
 if __name__ == "__main__":
